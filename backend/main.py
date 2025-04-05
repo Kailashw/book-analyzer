d3653b30c278eef10ec826b3b6a8e8137eabc35e
Kailas Walldoddi <kailashwall@gmail.com>
2025-04-05 07:05:47 +0530

    changed cors policies

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://book-analyzer-psi.vercel.app"
-    ],
+    allow_origins=[ "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
